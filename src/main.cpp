be83137d405e4df9c8382fc0c0bf417100770b68
sai <kakubalsai@gmail.com>
2021-04-10 02:15:16 +0200

    remove unused variables

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #include "../include/display.h"
 
 int main() {
-    constexpr std::size_t kFramesPerSecond{60};
-    constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
     constexpr std::size_t kScreenDim{600}; // only square windows
     constexpr double cxMin{-2.2}, cxMax{1.2}, cyMin{-1.7}, cyMax{1.7};
     constexpr std::size_t maxIter{600}; // max iterations
